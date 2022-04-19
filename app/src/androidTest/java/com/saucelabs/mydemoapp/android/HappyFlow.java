63939634db2b4b5da0610eabe0b47272d4df5419
Diego Perini <diego.perini@saucelabs.com>
2022-04-19 15:59:23 +0300

    Fixed runtime retention of test annotations

--- app/src/androidTest/java/com/saucelabs/mydemoapp/android/HappyFlow.java
+++ app/src/androidTest/java/com/saucelabs/mydemoapp/android/HappyFlow.java
@@ -1,8 +1,11 @@
 package com.saucelabs.mydemoapp.android;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.METHOD})
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface HappyFlow {
 }
