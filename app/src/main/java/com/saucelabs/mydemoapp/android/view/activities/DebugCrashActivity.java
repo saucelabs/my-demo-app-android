127872db779247812835de3ab0027cebed8c7940
Gil Megidish <gil.megidish@saucelabs.com>
2022-04-30 23:21:15 +0300

    Added crash debug menu

---
+++ app/src/main/java/com/saucelabs/mydemoapp/android/view/activities/DebugCrashActivity.java
@@ -0,0 +1,63 @@
+package com.saucelabs.mydemoapp.android.view.activities;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.databinding.DataBindingUtil;
+
+import com.saucelabs.mydemoapp.android.MyApplication;
+import com.saucelabs.mydemoapp.android.R;
+import com.saucelabs.mydemoapp.android.databinding.ActivityDebugCrashBinding;
+import com.saucelabs.mydemoapp.android.databinding.ActivityDebugFeedbackBinding;
+import com.saucelabs.mydemoapp.android.utils.TestFairyAssetReader;
+import com.saucelabs.mydemoapp.android.utils.base.BaseActivity;
+import com.testfairy.FeedbackFormField;
+import com.testfairy.FeedbackOptions;
+import com.testfairy.SelectFeedbackFormField;
+import com.testfairy.StringFeedbackFormField;
+import com.testfairy.TestFairy;
+import com.testfairy.TextAreaFeedbackFormField;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import backtraceio.library.BacktraceClient;
+
+public class DebugCrashActivity extends BaseActivity {
+	private ActivityDebugCrashBinding binding;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		binding = DataBindingUtil.setContentView(this, R.layout.activity_debug_crash);
+		initialize();
+	}
+
+	private void initialize() {
+
+		binding.causeUncaughtExceptionButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (MyApplication.backtraceClient != null) {
+					throw new RuntimeException("You clicked on the wrong button!");
+				}
+			}
+		});
+
+		binding.causeNativeCrashButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (MyApplication.backtraceClient == null) {
+					Toast.makeText(DebugCrashActivity.this, "Backtrace client not initialized, cannot crash app", Toast.LENGTH_LONG).show();
+					return;
+				}
+
+				MyApplication.backtraceClient.nativeCrash();
+			}
+		});
+	}
+}
