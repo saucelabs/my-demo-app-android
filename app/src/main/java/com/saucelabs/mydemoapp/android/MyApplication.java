42702af9343703c80c2b3fa3b5f25eeaf26b8649
Gil Megidish <gil.megidish@saucelabs.com>
2022-05-03 20:18:18 +0300

    Backtrace sample code

--- app/src/main/java/com/saucelabs/mydemoapp/android/MyApplication.java
+++ app/src/main/java/com/saucelabs/mydemoapp/android/MyApplication.java
@@ -1,8 +1,11 @@
 package com.saucelabs.mydemoapp.android;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.*;
 
 import android.content.Context;
+import android.os.Build;
+import android.util.Log;
 
 import com.testfairy.TestFairy;
 import com.testfairy.SessionStateListener;
@@ -10,9 +13,14 @@ import com.saucelabs.mydemoapp.android.utils.Network;
 import com.saucelabs.mydemoapp.android.utils.SingletonClass;
 import com.saucelabs.mydemoapp.android.utils.TestFairyAssetReader;
 
+import backtraceio.library.BacktraceClient;
+import backtraceio.library.BacktraceCredentials;
+import backtraceio.library.models.BacktraceExceptionHandler;
+
 public class MyApplication extends android.app.Application {
 
 	public static MyApplication instance;
+	public static BacktraceClient backtraceClient;
 
 	@Override
 	public void onCreate() {
@@ -21,6 +29,7 @@ public class MyApplication extends android.app.Application {
 		SingletonClass.getInstance();
 
 		initializeTestFairy();
+		initializeBacktrace();
 
 		DeviceVitalsDemo demo = new DeviceVitalsDemo();
 		demo.kickstart();
@@ -33,6 +42,20 @@ public class MyApplication extends android.app.Application {
 
 	public static MyApplication getInstance() {
 		return instance;
+	}
+
+	private void initializeBacktrace() {
+		/*
+		BacktraceCredentials credentials = new BacktraceCredentials(
+			"<url>",
+			"<token>"
+		);
+
+		BacktraceClient backtraceClient = new BacktraceClient(getApplicationContext(), credentials);
+		BacktraceExceptionHandler.enable(backtraceClient);
+
+		MyApplication.backtraceClient = backtraceClient;
+		*/
 	}
 
 	private void initializeTestFairy() {
@@ -51,7 +74,7 @@ public class MyApplication extends android.app.Application {
 		TestFairy.begin(this, testFairyData.getAppToken());
 	}
 
-	private String getRandomUserId() {
+	private static String getRandomUserId() {
 		Random random = new Random();
 		String[] names = new String[]{
 			"oliver", "william", "james", "benjamin", "henry", "diego", "alexander", "guy",
