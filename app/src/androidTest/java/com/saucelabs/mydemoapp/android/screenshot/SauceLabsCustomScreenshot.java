2cf86c451662241a06f0b18d4050e2186493eebb
Gil Megidish <gil.megidish@saucelabs.com>
2022-08-25 17:46:56 +0300

    Indentations

--- app/src/androidTest/java/com/saucelabs/mydemoapp/android/screenshot/SauceLabsCustomScreenshot.java
+++ app/src/androidTest/java/com/saucelabs/mydemoapp/android/screenshot/SauceLabsCustomScreenshot.java
@@ -13,42 +13,42 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class SauceLabsCustomScreenshot {
 
-    private static final AtomicInteger screenshotId = new AtomicInteger(1);
+	private static final AtomicInteger screenshotId = new AtomicInteger(1);
 
-    public static void capture(String screenshotName) {
-        try {
-            screenshotName = String.format("%04d-%s", screenshotId.getAndIncrement(), screenshotName);
+	public static void capture(String screenshotName) {
+		try {
+			screenshotName = String.format("%04d-%s", screenshotId.getAndIncrement(), screenshotName);
 
-            ScreenCapture capture = Screenshot.capture();
-            capture.setFormat(Bitmap.CompressFormat.PNG);
-            capture.setName(screenshotName);
+			ScreenCapture capture = Screenshot.capture();
+			capture.setFormat(Bitmap.CompressFormat.PNG);
+			capture.setName(screenshotName);
 
-            SauceLabsScreenCaptureProcessor processor = new SauceLabsScreenCaptureProcessor();
-            processor.process(capture);
+			SauceLabsScreenCaptureProcessor processor = new SauceLabsScreenCaptureProcessor();
+			processor.process(capture);
 
-            String savedScreenshotPath = new File(processor.getPathForFile(screenshotName) + ".png").getPath();
+			String savedScreenshotPath = new File(processor.getPathForFile(screenshotName) + ".png").getPath();
 
-            Log.i("SauceLabs", String.format("Screenshot saved to %s", savedScreenshotPath));
-        } catch (Exception e) {
-            Log.e("SauceLabs", "Failed to capture screenshot", e);
-        }
-    }
+			Log.i("SauceLabs", String.format("Screenshot saved to %s", savedScreenshotPath));
+		} catch (Exception e) {
+			Log.e("SauceLabs", "Failed to capture screenshot", e);
+		}
+	}
 
-    private static class SauceLabsScreenCaptureProcessor extends BasicScreenCaptureProcessor {
+	private static class SauceLabsScreenCaptureProcessor extends BasicScreenCaptureProcessor {
 
-        private static final String SAUCE_LABS_CUSTOM_SCREENSHOTS = "sauce_labs_custom_screenshots";
+		private static final String SAUCE_LABS_CUSTOM_SCREENSHOTS = "sauce_labs_custom_screenshots";
 
-        public SauceLabsScreenCaptureProcessor() {
-            super();
+		public SauceLabsScreenCaptureProcessor() {
+			super();
 
-            File downloadsDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
-            File screenshotsDir = new File(downloadsDir, SAUCE_LABS_CUSTOM_SCREENSHOTS);
-            this.mDefaultScreenshotPath = screenshotsDir;
-        }
+			File downloadsDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+			File screenshotsDir = new File(downloadsDir, SAUCE_LABS_CUSTOM_SCREENSHOTS);
+			this.mDefaultScreenshotPath = screenshotsDir;
+		}
 
-        public String getPathForFile(String name) {
-            File file = new File(mDefaultScreenshotPath, getFilename(name));
-            return file.getAbsolutePath();
-        }
-    }
+		public String getPathForFile(String name) {
+			File file = new File(mDefaultScreenshotPath, getFilename(name));
+			return file.getAbsolutePath();
+		}
+	}
 }
